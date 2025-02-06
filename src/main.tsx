f76360abc852509ae0fa88d41b478cd7548aa6d0
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-05 20:23:45 -0800

    validating

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+if(!navigator.geolocation) {
+  alert("Imposible obtener tu ubicación.");
+
+  throw new Error("Imposible obtener tu ubicación.");
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
