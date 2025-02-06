7beb552e54951352ea15a32a0c015582aa9c26a8
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-05 19:53:51 -0800

    new project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import "./index.css"
+
+function App() {
+  return (
+    <div className="bg-neutral-900">
+      <h1 className="text-red-500">Hello</h1>
+      <Button>Hey</Button>
+    </div>
+  )
+}
+
+export default App
