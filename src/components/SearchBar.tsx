917c168553280bfe426906046556b850c8297e68
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-06 00:17:15 -0800

    finishing searchboar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,24 +1,25 @@
-import { ChangeEvent, useRef, useState, useEffect, useContext } from 'react';
-import MaplibreGeocoder, { MaplibreGeocoderFeatureResults, CarmenGeojsonFeature } from '@maplibre/maplibre-gl-geocoder';
+import { ChangeEvent, useRef, useEffect, useContext } from 'react';
+import MaplibreGeocoder, { MaplibreGeocoderFeatureResults, } from '@maplibre/maplibre-gl-geocoder';
 import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css';
 import maplibregl from "maplibre-gl";
 
-import { geocoderApi } from '@/api/geocoder';
 import { MapContext } from '@/stores/MapContext';
+import { PlacesContext } from '@/stores/PlacesContext';
+
+import { geocoderApi } from '@/api/geocoder';
+
 import { Input } from '@/components/ui/input';
 
 export const SearchBar = () => {
+  const { map } = useContext(MapContext);
+  const { setPlaces, places } = useContext(PlacesContext);
   const debounceRef = useRef<NodeJS.Timeout>(null);
   const geocoderRef = useRef<MaplibreGeocoder | null>(null);
-  const { map } = useContext(MapContext);
-  const [results, setResults] = useState<CarmenGeojsonFeature[]>([]);
-  const [mapLoaded, setMapLoaded] = useState(false);
 
   useEffect(() => {
     if (!map) return;
     
     const onLoad = () => {
-      setMapLoaded(true);
       geocoderRef.current = new MaplibreGeocoder(geocoderApi, {
         proximity: { longitude: map.getCenter().lat, latitude: map.getCenter().lng },
         placeholder: 'Buscar un lugar...',
@@ -30,7 +31,7 @@ export const SearchBar = () => {
       geocoderRef.current.on('results', (e: MaplibreGeocoderFeatureResults) => {
         const features = e.features ?? [];
         console.log("event", features);
-        setResults(features);
+        setPlaces(features);
       });
     };
 
@@ -43,7 +44,7 @@ export const SearchBar = () => {
     return () => {
       map.off('load', onLoad);
       geocoderRef.current?.off('results', () => {
-        setResults([]);
+        setPlaces([]);
       });
     };
   }, [map]);
@@ -52,9 +53,10 @@ export const SearchBar = () => {
     if (debounceRef.current) {
       clearTimeout(debounceRef.current);
     }
+
     debounceRef.current = setTimeout(() => {
       const query = event.target.value;
-      if (geocoderRef.current && mapLoaded) {
+      if (geocoderRef.current && map) {
         geocoderRef.current.query(query);
       }
     }, 600);
@@ -63,9 +65,9 @@ export const SearchBar = () => {
   return (
     <div className="fixed top-10 left-10 z-10 bg-neutral-900 drop-shadow-lg rounded-lg p-2">
       <Input type="text" placeholder="Busca un lugar..." onChange={onQueryChanged} />
-      {results.length > 0 && (
+      {places?.length! > 0 && (
         <ul className="mt-2 z-10">
-          {results.map((feature) => (
+          {places?.map((feature) => (
             /* @ts-ignore next-line */
             <li key={feature.id} className="text-neutral-100 cursor-pointer hover:bg-neutral-800 p-1" onClick={() => map?.flyTo({ center: { lat: feature.geometry.coordinates[1], lng: feature.geometry.coordinates[0] }, zoom: 16 })}>
               {feature.place_name}
