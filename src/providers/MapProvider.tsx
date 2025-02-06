c59d75187383aebfa91de81f6d220c18b288e30b
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-05 22:16:05 -0800

    showing marker in user location

--- src/providers/MapProvider.tsx
+++ src/providers/MapProvider.tsx
@@ -1,8 +1,9 @@
-import { FC, PropsWithChildren, useReducer } from 'react'
-import { Map } from "maplibre-gl";
+import { FC, PropsWithChildren, useContext, useReducer } from 'react'
+import { Map, Marker, Popup } from "maplibre-gl";
 
 import { MapContext } from '@/stores/MapContext';
 import { mapReducer } from '@/stores/MapReducer';
+import { PlacesContext } from '@/stores/PlacesContext';
 
 export interface MapState {
   isLoading: boolean;
@@ -16,9 +17,22 @@ const INITIAL_STATE: MapState = {
 
 export const MapProvider: FC<PropsWithChildren> = ({ children }) => {
   const [state, dispatch] = useReducer(mapReducer, INITIAL_STATE);
+  const { userLocation } = useContext(PlacesContext);
 
-  const setMap = (payload: Map) => {
-    dispatch({ type: 'setMap', payload });
+  const setMap = (map: Map) => {
+    const myLocationPopUp = new Popup()
+      .setHTML(`
+      <h3 className="text-red-500">Tu Ubicación</h3>
+      `);
+
+    new Marker({
+      color: "#f44336",
+    })
+    .setLngLat(userLocation || map.getCenter())
+    .setPopup(myLocationPopUp)
+    .addTo(map);
+    
+    dispatch({ type: 'setMap', payload: map });
   }
 
   return (
