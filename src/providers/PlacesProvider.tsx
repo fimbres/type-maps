9989e07949afc135552dddf2eba72e61e323a24b
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-06 00:26:10 -0800

    adding places

--- src/providers/PlacesProvider.tsx
+++ src/providers/PlacesProvider.tsx
@@ -1,4 +1,5 @@
 import { FC, PropsWithChildren, useEffect, useReducer } from "react";
+import { CarmenGeojsonFeature } from '@maplibre/maplibre-gl-geocoder';
 
 import { PlacesContext } from "@/stores/PlacesContext";
 import { placesReducer } from "@/stores/PlacesReducer";
@@ -7,16 +8,22 @@ import { getUserLocation } from "@/lib/getUserLocation";
 
 export interface PlacesState {
   isLoading: boolean;
+  places: CarmenGeojsonFeature[];
   userLocation?: [number, number];
 }
 
 const INITIAL_STATE: PlacesState = {
   isLoading: true,
+  places: [],
   userLocation: undefined,
 };
 
 export const PlacesProvider: FC<PropsWithChildren> = ({ children }) => {
   const [state, dispatch] = useReducer(placesReducer, INITIAL_STATE);
+
+  const setPlaces = (payload: CarmenGeojsonFeature[]) => {
+    dispatch({ type: 'setPlaces', payload });
+  }
 
   useEffect(() => {
     getUserLocation()
@@ -24,7 +31,7 @@ export const PlacesProvider: FC<PropsWithChildren> = ({ children }) => {
   }, []);
 
   return (
-    <PlacesContext.Provider value={{ ...state }}>
+    <PlacesContext.Provider value={{ ...state, setPlaces }}>
       {children}
     </PlacesContext.Provider>
   )
